b6ac3a8b5e5058437028ab7fa6eda7d73a35a24f
Satyam Thakur <sat.thakur20@gmail.com>
2023-08-16 17:03:46 +0530

    Adding javascript basics directory

---
+++ javascript_basics/main.js
@@ -0,0 +1,16 @@
+let a = 10
+var b = 20
+
+console.log(a)
+console.log(b)
+
+// function fun(a) {
+//     if (a == 0)
+//         return
+
+//     console.log(a)
+
+//     fun(a-1)
+// }
+
+// fun(5)
