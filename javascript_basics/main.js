412a5b7cb45348ad67d3c9eed280c12022f85aad
Satyam Thakur <sat.thakur20@gmail.com>
2023-08-16 17:30:58 +0530

    Using an external js file to alert

--- javascript_basics/main.js
+++ javascript_basics/main.js
@@ -1,15 +1,16 @@
-function fun(a) {
-    if (a == 0)
-        return
+// function fun(a) {
+//     if (a == 0)
+//         return
 
-    console.log(a)
+//     console.log(a)
 
-    fun(a-1)
-}
+//     fun(a-1)
+// }
 
-let a = 10
-var b = 20
+// let a = 10
+// var b = 20
 
-console.log(a)
-console.log(b)
+// console.log(a)
+// console.log(b)
 
+alert("Hello, world");
