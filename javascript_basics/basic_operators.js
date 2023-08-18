a91c286dac225e4a2174a184c378856865ffd843
Satyam Thakur <sat.thakur20@gmail.com>
2023-08-18 10:20:21 +0530

    Spread Operator in JS

---
+++ javascript_basics/basic_operators.js
@@ -0,0 +1,15 @@
+var a = 5
+var b = 8
+var sum = a + b
+var mul = a * b
+var div = b / a;
+var sub = b - a;
+
+var con = 5 + "Hello"
+
+console.log(sum)
+console.log(mul)
+console.log(div)
+console.log(sub)
+
+console.log(con)
---
+++ javascript_basics/special_operators.js
@@ -0,0 +1,8 @@
+function sum(a, b, c) {
+    console.log(a+b+c)
+}
+
+var arr = [5, 5, 3];
+console.log(...arr)
+
+sum(...arr)
