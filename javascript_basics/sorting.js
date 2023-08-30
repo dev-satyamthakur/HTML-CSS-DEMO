8bab30724f641d7c2f8e20fb67cd22651bc6facc
Satyam Thakur <sat.thakur20@gmail.com>
2023-08-30 10:40:28 +0530

    Adding a comparator function to sort an array of int

--- javascript_basics/sorting.js
+++ javascript_basics/sorting.js
@@ -1,5 +1,9 @@
-const arr = [3, 4, 2, 1, 7, 8, 5, 0, 9, 6]
-arr.sort()
+function cmp(a, b) {
+    return a - b;
+}
+
+const arr = [3, 4, 2, 1, 700, 8, 5, 0, 9, 6]
+arr.sort(cmp)
 console.log(arr)
 
 const fruits = ["apple", "mango", "orange", "grape", "cherry"]
